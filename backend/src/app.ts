51c51aec99258db7955828d945ccbf607dd4a350
Eike Haß <eike-hass@web.de>
2021-07-02 23:41:14 +0200

    make backend serve frontend

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -76,14 +76,14 @@ app.use((req, res, next) => {
     next();
 });
 
-app.use(
-    express.static(path.join(__dirname, "public"), { maxAge: 31557600000 })
+app.use("/",
+    express.static(path.join(__dirname,"../../frontend/build/"), { maxAge: 31557600000 })
 );
 
 /**
  * Primary app routes.
  */
-app.get("/", homeController.index);
+//app.get("/", homeController.index);
 app.get("/login", userController.getLogin);
 app.post("/login", userController.postLogin);
 app.get("/logout", userController.logout);
