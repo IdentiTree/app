f735e5f9eab5762c1f4dbb203415f6b4f8f970d3
huhn511 <sehe89@gmail.com>
2021-07-03 18:46:09 +0200

    add cli wallet and api

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -6,6 +6,7 @@ import bluebird from "bluebird";
 import { MONGODB_URI } from "./util/secrets";
 import { connectToMongo } from './config/mongo';
 import userRoutes from './routes/userRoutes';
+import walletRoutes from './routes/walletRoutes';
 import { errorHandler } from './middleware/errors';
 
 
@@ -26,6 +27,7 @@ app.use(compression());
 app.use(express.json());
 
 app.use('/api/users', userRoutes);
+app.use('/api/wallet', walletRoutes);
 app.use("/",
     express.static(path.join(__dirname,"../../frontend/build/"), { maxAge: 31557600000 })
 );
