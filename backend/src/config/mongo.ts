6f15a8afa2ed61fbdd546bf4bba11f1720e934a4
Coodos <info@coodos.co>
2021-07-03 13:51:15 +0530

    cleaned up mongo connection

---
+++ backend/src/config/mongo.ts
@@ -0,0 +1,24 @@
+import mongoose from 'mongoose';
+
+/**
+ * connect to the database running on the connection string provided
+ *
+ * @param   {String} connectionString : Mongo DB Connection string
+ */
+
+const connectToMongo = async (connectionString: string): Promise<void> => {
+  try {
+    // await connection from mongo
+    const connection = await mongoose.connect(connectionString, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+      useCreateIndex: true
+    });
+    console.log(`--> Connected to MongoDB on ${connection.connection.host}`)
+  } catch (error) {
+    console.error(`Can't connect to MongoDB: ${error}`);
+    process.exit(1);
+  }
+}
+
+export { connectToMongo }
