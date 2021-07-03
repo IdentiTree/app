2f4ec7c86b33ebb0cb39d910bedd22f357942187
Coodos <info@coodos.co>
2021-07-03 21:04:15 +0530

    added auth middleware

---
+++ backend/src/middleware/auth.ts
@@ -0,0 +1,40 @@
+import jwt from 'jsonwebtoken';
+import { User } from '../models/User';
+import asyncHandler from 'express-async-handler';
+import { Request, Response } from 'express';
+import { NextFunction } from 'express-serve-static-core';
+
+/**
+ * make sure that the user is authorized by checking if an
+ * access token exists and if it does then set the req.user
+ * varibale to that
+ */
+
+const ensureAuthorized = asyncHandler(
+  async (req: Request, res: Response, next: NextFunction) => {
+    let token: any;
+    if (
+      req.headers.authorization &&
+      req.headers.authorization.startsWith('Bearer')
+    ) {
+      try {
+        token = req.headers.authorization.split(' ')[1];
+        const JWT_SECRET: any = process.env.JWT_SECRET;
+        const decoded: any = jwt.verify(token, JWT_SECRET);
+        req.user = await User.findById(decoded.id);
+        next();
+      } catch (error) {
+        console.error(error);
+        res.status(401);
+        throw new Error('Not authorized, token failed');
+      }
+    }
+
+    if (!token) {
+      res.status(401);
+      throw new Error('Not logged in / missing token');
+    }
+  }
+);
+
+export { ensureAuthorized };
