f95c95e2ff25750222fbdd55c2125b6e700883c8
Coodos <info@coodos.co>
2021-07-03 20:55:25 +0530

    added express declaration, dummy data and DID

---
+++ backend/src/data/seeder.ts
@@ -0,0 +1,25 @@
+import { users } from "./sampleData/usersData";
+import { encrypt } from "../util/crypto";
+import { createDID } from "../util/did.js";
+import { User } from '../models/User';
+import { connectToMongo } from '../config/mongo';
+import { MONGODB_URI } from '../util/secrets';
+
+connectToMongo(MONGODB_URI);
+
+users.map(async (user: any) => {
+    const DID_Data = await createDID();
+    const encryptedPrivKey = encrypt(DID_Data.key.secret);
+    const newUser: any = await User.create({
+        username: user.username,
+        password: user.password,
+        did: DID_Data.did,
+        key: {
+            pub: DID_Data.key.public,
+            priv: encryptedPrivKey
+        }
+    }).catch((error) => {
+        console.error(`unable to create username ${user.username}`);
+    });
+    newUser ? console.log(`created new user : ${newUser._id}`) : null;
+});
