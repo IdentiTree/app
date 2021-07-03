cd68562b1f40ac6604d293cacd6d55d637d0b125
Daniel Mader <45594838+daniel-mader@users.noreply.github.com>
2021-07-04 00:10:08 +0200

    feat: Add basic components

---
+++ frontend/src/components/areas/Areas.tsx
@@ -0,0 +1,26 @@
+import BaseLayout from "../../layout/BaseLayout";
+import AreaEntry from "./AreaEntry";
+import {makeStyles} from "@material-ui/core";
+
+const useStyles = makeStyles({
+    root: {
+        display: "flex",
+        justifyContent: "center",
+        flexDirection: "column",
+    },
+});
+
+export default function Areas() {
+    const classes = useStyles();
+
+    return (
+        <BaseLayout>
+            <h1>Areas</h1>
+            <div className={classes.root}>
+                <AreaEntry title={"Area 1"} name={"Evergreen Forest"} carbonAmount={1.2} area={3.4}/>
+                <AreaEntry title={"Area 2"} name={"Black Forest"} carbonAmount={3.2} area={0.4}/>
+                <AreaEntry title={"Area 3"} name={"Tierpark Berlin"} carbonAmount={13.2} area={53.4}/>
+            </div>
+        </BaseLayout>
+    )
+};
