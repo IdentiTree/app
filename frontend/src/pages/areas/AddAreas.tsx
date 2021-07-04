41e41a10438b6e44d81151a21d29c71865f94ac2
Eike Haß <eike-hass@web.de>
2021-07-04 15:36:04 +0200

    added name to endpoint

--- frontend/src/pages/areas/AddAreas.tsx
+++ frontend/src/pages/areas/AddAreas.tsx
@@ -34,7 +34,7 @@ export default function AddArea() {
               'Content-Type': 'application/json'
             },
             body: JSON.stringify({
-                "name": "tiergarten",
+                "name": name,
                 "coordinates": [newArea],
                 "area": area / 10000,
                 "biomes": areaTypes,
