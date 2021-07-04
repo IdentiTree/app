2a9b445b8f5f746606eefb7543eca189c0360158
Eike Haß <eike-hass@web.de>
2021-07-04 12:51:54 +0200

    added drawer

--- frontend/src/pages/areas/AddArea.tsx
+++ frontend/src/pages/areas/AddArea.tsx
@@ -1,6 +1,8 @@
 import BaseLayout from "../../layout/BaseLayout";
 import Map from "../../components/Map";
-import {Box, Button, makeStyles, Typography} from "@material-ui/core";
+import {Box, Button, Drawer, makeStyles, MenuItem, Select, Typography} from "@material-ui/core";
+import { useState } from "react";
+import type { LatLngExpression } from 'leaflet';
 
 const useStyles = makeStyles({
     saveButton: {
@@ -11,6 +13,8 @@ const useStyles = makeStyles({
 
 export default function AddArea() {
     const classes = useStyles();
+    const [newArea, setNewArea] = useState<LatLngExpression[]>([]); 
+    const [openDrawer, setOpenDrawer] = useState<boolean>(false); 
 
     return (
         <BaseLayout>
@@ -21,13 +25,27 @@ export default function AddArea() {
                     <Map
                         mode="draw"
                         center={[51.505, -0.09]}
-                        onAreaCreate={(area) => console.log(area)}
+                        onAreaCreate={(area) => setNewArea(area)}
                     />
                 </Box>
             </Box>
             <div className={classes.saveButton}>
-                <Button variant="contained" color="primary" disabled={true}>Add area</Button>
+                <Button variant="contained" color="primary" disabled={!newArea.length} onClick={() => setOpenDrawer(true)}>Add area</Button>
             </div>
+            <Drawer anchor={'bottom'} open={openDrawer} onClose={() => setOpenDrawer(false)}>
+                <Box height="50vh">
+                <Select
+                    labelId="demo-simple-select-label"
+                    id="demo-simple-select"
+                    value={10}
+                    onChange={() => undefined}
+                    >
+                    <MenuItem value={10}>Ten</MenuItem>
+                    <MenuItem value={20}>Twenty</MenuItem>
+                    <MenuItem value={30}>Thirty</MenuItem>
+                    </Select>
+                </Box>
+            </Drawer>
         </BaseLayout>
     )
 }
