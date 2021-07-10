2757e80396114bd4bb52d5f6805a12a1ec8f77dd
huhn511 <sehe89@gmail.com>
2021-07-10 02:12:24 +0200

    add login_did with demo did auth

--- backend/src/controllers/userControllers.ts
+++ backend/src/controllers/userControllers.ts
@@ -3,6 +3,8 @@ import asyncHandler from "express-async-handler";
 import { User } from '../models/User';
 import { Request, Response } from 'express';
 import { tokenize } from '../util/jwt';
+import { authenticate } from '../util/did';
+import console from "console";
 
 const userLogin = asyncHandler(async (req: Request, res: Response) => {
     interface IUserLoginProps {
@@ -29,4 +31,46 @@ const userLogin = asyncHandler(async (req: Request, res: Response) => {
     }
 });
 
-export { userLogin };
+
+// Login method with IOTA Identity
+const userLoginDID = asyncHandler(async (req: Request, res: Response) => {
+
+    const credential = req.body;
+
+    console.log("credential", credential)
+
+    let response: any = {}
+    response = await authenticate(credential)
+    console.log("response", response)
+
+    if (response.result.verified) {
+
+        // check if credential is new
+
+        var issuanceDate = new Date(response.result.credential.issuanceDate).getTime();
+        var nowDate = new Date().getTime();
+        var withinRange = nowDate - issuanceDate
+
+        console.log("issuanceDate", issuanceDate)
+        console.log("nowDate", nowDate)
+        console.log("withinRange", withinRange)
+
+        if (withinRange <= 10000) {
+            console.log("authenticate success!")
+
+            const token: string = tokenize("user._id");
+            res.json({
+                token
+            });
+        } else {
+            res.json({
+                error: "credential to old"
+            });
+        }
+    } else {
+        res.status(403);
+        throw new Error('Incorrect credential');
+    }
+});
+
+export { userLogin, userLoginDID };
